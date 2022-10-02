479d3586bc2d1453ca97a1235f36098014a9dd0d
John Aughey <jha@aughey.com>
2022-10-02 11:26:42 -0500

    push to

--- PrototypeCopy/Program.cs
+++ PrototypeCopy/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+ var obj = new System.Windows.Forms.DataObject(
+            DataFormats.Text,
+            "This is a test"
+        );
+
+await StaHelper.Run(() => Clipboard.SetDataObject( obj, true ));
---
+++ PrototypeCopy/StaHelper.cs
@@ -0,0 +1,51 @@
+class StaHelper
+{
+    readonly SemaphoreSlim done = new(0);
+    readonly Action Work;
+
+    StaHelper(Action work) {
+        Work = work;
+    }
+
+    public static async Task Run(Action work) {
+        var helper = new StaHelper(work);
+        helper.Go();
+        await helper.done.WaitAsync();
+    }
+
+    public void Go()
+    {
+        var thread = new Thread( new ThreadStart( DoWork ) )
+        {
+            IsBackground = true,
+        };
+        #pragma warning disable CA1416
+        thread.SetApartmentState( ApartmentState.STA );
+        #pragma warning restore CA1416
+
+        thread.Start();
+    }
+
+    // Thread entry method
+    private void DoWork()
+    {
+        try
+        {
+            Work();
+        }
+        catch( Exception ex )
+        {
+          
+          
+                    // ex from first exception
+                    System.Console.WriteLine( ex );
+             
+        }
+        finally
+        {
+            done.Release();
+        }
+    }
+
+
+}
