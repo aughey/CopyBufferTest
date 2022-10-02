69d70da31e3affd1e9e9abe40d6318717f3ac926
John Aughey <jha@aughey.com>
2022-10-02 16:41:13 +0000

    sta

--- PrototypeCopy/StaHelper.cs
+++ PrototypeCopy/StaHelper.cs
@@ -1,13 +1,19 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
 class StaHelper
 {
     readonly SemaphoreSlim done = new(0);
     readonly Action Work;
 
-    StaHelper(Action work) {
+    StaHelper(Action work)
+    {
         Work = work;
     }
 
-    public static async Task Run(Action work) {
+    public static async Task Run(Action work)
+    {
         var helper = new StaHelper(work);
         helper.Go();
         await helper.done.WaitAsync();
@@ -15,13 +21,13 @@ class StaHelper
 
     public void Go()
     {
-        var thread = new Thread( new ThreadStart( DoWork ) )
+        var thread = new Thread(new ThreadStart(DoWork))
         {
             IsBackground = true,
         };
-        #pragma warning disable CA1416
-        thread.SetApartmentState( ApartmentState.STA );
-        #pragma warning restore CA1416
+#pragma warning disable CA1416
+        thread.SetApartmentState(ApartmentState.STA);
+#pragma warning restore CA1416
 
         thread.Start();
     }
@@ -33,13 +39,10 @@ class StaHelper
         {
             Work();
         }
-        catch( Exception ex )
+        catch (Exception ex)
         {
-          
-          
-                    // ex from first exception
-                    System.Console.WriteLine( ex );
-             
+            // ex from first exception
+            System.Console.WriteLine(ex);
         }
         finally
         {
